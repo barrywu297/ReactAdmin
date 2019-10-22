50d1887657595546a386e19557b622d12f3742d1
doufu297 <0027011563@iwhalecloud.com>
2019-10-22 15:15:16 +0800

    10.21修改

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { Button, message } from 'antd';
 /** 应用根组件 */
 
 export default class App extends Component {
+    handleClick = () => {
+        message.success("成功了！");
+    }
     render() {
-        return <div>App</div>;
+        return <Button type="primary" onClick={this.handleClick}>测试antd</Button>;
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//引入antd的全局样式
+// import 'antd/dist/antd.css'
+
 import App from './App'
 
 //将App组件标签渲染到index页面的div上
